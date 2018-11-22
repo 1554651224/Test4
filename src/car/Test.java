1c1695b44e553345373667655577be7da74f8774
1554651224 <1554651224@qq.com>
2018-11-22 19:10:05 +0800

    3

--- src/car/Test.java
+++ src/car/Test.java
@@ -46,9 +46,7 @@ public class Test {
 		Car[] cars = icd.queryBybrand("±£Ê±½Ý");
 		printCars(cars);
 		
-		System.out.println("=============queryByBrand=============");	
-		Car[] cars2 = icd.queryByType("X3");
-		printCars(cars2);
+		
 		
 	}	
 	
